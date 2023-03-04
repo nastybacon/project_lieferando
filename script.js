015ec8fc8b5eb072d36ec1f274a19e18e2577460
nastybacon <126672145+nastybacon@users.noreply.github.com>
2023-03-04 10:40:06 +0100

    shopping cart

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+window.onscroll = function(){ // beim Scrollen aktivieren
+    let shoppingCart = document.getElementById('shoppingCart'); // Variable definieren
+    console.log('Hallo');
+    if(window.scrollY > 0) { // sobald 1px bewegt
+        shoppingCart.style = 'top: 0'; // Einkaufswagen 0px Abstand
+    } else { // wenn nicht, dann 
+        shoppingCart.style = 'top: 100px'; // Abstand von Oben 100px
+    }
+}
